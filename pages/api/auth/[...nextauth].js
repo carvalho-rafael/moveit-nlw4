5280ce43b290aa8b9e1d5ffbd99515b714aec7fa
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-03-03 12:35:11 -0300

    save user to database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,4 +8,13 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET
     }),
   ],
+  database: {
+    type: 'postgres',
+    host: 'ec2-54-166-242-77.compute-1.amazonaws.com',
+    port: 5432,
+    username: 'gflxofreknmrup',
+    password: 'ac0c3b7a19323e438f14484e4f4588254aa397b081f51f4efc42da2004beef3a',
+    database: 'd558hv83f62bq3',
+    ssl: true
+  }
 })
