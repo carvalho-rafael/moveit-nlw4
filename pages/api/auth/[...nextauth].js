531cca8010d2104f290650d490fa2341aaa2807f
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-03-04 11:55:59 -0300

    change db connection

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,11 +10,11 @@ export default NextAuth({
   ],
   database: {
     type: 'postgres',
-    host: 'ec2-54-166-242-77.compute-1.amazonaws.com',
+    host: process.env.DATABASE_HOST,
     port: 5432,
-    username: 'gflxofreknmrup',
-    password: 'ac0c3b7a19323e438f14484e4f4588254aa397b081f51f4efc42da2004beef3a',
-    database: 'd558hv83f62bq3',
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
     ssl: true
   }
 })
